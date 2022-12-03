f0fe291dbb692d98f86c66504f78369be7905314
Annie0329 <0329annie@gmail.com>
2022-12-03 17:31:48 +0800

    1203

--- js/script.js
+++ js/script.js
@@ -5,12 +5,30 @@ $(document).ready(function () {
   const $nameField = $('#nameField');
   const $messageList = $('#messageList');
   const $sendButton = $('#sendButton')
+  var nameVal = $('#nameField option:selected').text();
 
+  console.log(nameVal);
   const $fontAvailable = document.fonts.check("16px A Childish Wonders");
   if ($fontAvailable) {
     console.log("yass");
   }
   //console.log($fontAvailable);
+  $('#nameField').change(
+    function () {
+      nameVal = $('#nameField option:selected').text();
+      console.log(nameVal);
+      if (nameVal == "Annie") {
+        $('.messageItemAnnie').css("text-align", "right");
+        $('.messageItemTai').css("text-align", "left");
+        $('.messageField,.sendButton,.title').css("font-family", "Writes_AnnieFont");
+      }
+      if (nameVal == "Tai") {
+        $('.messageItemAnnie').css("text-align", "left");
+        $('.messageItemTai').css("text-align", "right");
+        $('.messageField,.sendButton,.title').css("font-family", "writesTai");
+      }
+    }
+  )
 
   //按下enter就顯示訊息
   $messageField.keypress(function (e) {
@@ -20,14 +38,34 @@ $(document).ready(function () {
   });
   //按傳送鈕也可以
   $sendButton.click(function () {
-    let messageItem = `
-        <li class="messageItem">
-        <p class="message"><strong class="chatUsername">${$nameField.val()}:</strong>
-        ${$messageField.val()}</p>
-        <li>
-        `
-    $messageList.append(messageItem);
-    $messageField.val('')
+    nameVal = $('#nameField option:selected').text();
+    if (nameVal == "Annie") {
+
+      let messageItem = `
+      <li class="messageItemAnnie">
+      <p class="message"><strong class="chatUsername">${$nameField.val()}:</strong>
+      ${$messageField.val()}</p>
+      <li>
+      `
+
+      $messageList.append(messageItem);
+      $('.messageItemAnnie').css("text-align", "right");
+      $('.messageItemTai').css("text-align", "left");
+      $messageField.val('')
+    }
+    else {
+      let messageItem = `
+      <li class="messageItemTai">
+      <p class="message"><strong class="chatUsername">${$nameField.val()}:</strong>
+      ${$messageField.val()}</p>
+      <li>
+      `
+      $messageList.append(messageItem);
+      $('.messageItemTai').css("text-align", "right");
+      $('.messageItemAnnie').css("text-align", "left");
+      $messageField.val('')
+    }
+
     //訊息會自動滑到底
     $messageList[0].scrollTop = $messageList[0].scrollHeight;
   })
