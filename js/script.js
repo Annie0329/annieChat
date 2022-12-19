c35a8ae2cc724eda8d8aa857f4244cd51f040cf6
Annie0329 <0329annie@gmail.com>
2022-12-20 00:32:05 +0800

    1220

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
 $(document).ready(function () {
 
-  // 我不會JS，這是我自己看範例掰的，沒想到竟然成功
+  // INITIALIZE FIREBASE
+  firebase.initializeApp({
+    apiKey: "AIzaSyC-1xHrmDqqJYUah6b4W535SOtGoYwIyvY",
+    authDomain: "web1209-48c0a.firebaseapp.com",
+    projectId: "web1209-48c0a",
+    storageBucket: "web1209-48c0a.appspot.com",
+    messagingSenderId: "649891562731",
+    appId: "1:649891562731:web:df7980843b3c8e818deaae"
+  });
+
+  // REFERENCE CHATROOM DOCUMENT
+  let chatroomDocRef = firebase.firestore()
+    .collection("chatrooms")
+    .doc("chatroom1");
+  // REFERENCE CHATROOM MESSAGES
+  let messagesCollectionRef
+    = chatroomDocRef.collection("messages");
+  // QUERY MESSAGES BY TIMESTAMP ORDERING
+  let queryMessagesCollectionRef
+    = messagesCollectionRef.orderBy("timeStamp", "asc");
+
+  // REGISTER DOM ELEMENTS
   const $messageField = $('#messageField');
   const $nameField = $('#nameField');
   const $messageList = $('#messageList');
-  const $sendButton = $('#sendButton')
-  var nameVal = $('#nameField option:selected').text();
+  const $sendButton = $('#sendButton');
 
-  //如果切換使用者的話就換格式
-  $nameField.change(
+  // LISTEN FOR KEYPRESS EVENT
+  $messageField.keypress(function (e) {
+    if (e.keyCode == 13) {
+      $sendButton.click();
+    }
+  });
+
+  $('#nameField').change(
     function () {
       nameVal = $('#nameField option:selected').val();
 
@@ -18,46 +44,71 @@ $(document).ready(function () {
 
       if (nameVal == "Annie")
         $('.messageField,.sendButton,.nameField,.title,option').css("font-family", "Writes_AnnieFont");
-      else
+      if (nameVal == "Tai")
         $('.messageField,.sendButton,.nameField,.title,option').css("font-family", "writesTai");
 
     }
   )
 
-  //按下enter就顯示訊息
-  $messageField.keypress(function (e) {
-    if (e.keyCode == 13) {
-      $sendButton.click();
-    }
-  });
-
-  //按傳送鈕也可以
+  //按按鈕傳訊息
   $sendButton.click(function () {
-    //訊息不是空的才傳訊息
-    if ($messageField.val() != "") {
-      nameVal = $('#nameField option:selected').val();
-      nameValName = $('#nameField option:selected').text();
-
-      if (nameVal == "Annie")
-        liClass = "messageItemAnnieR";
-      else
-        liClass = "messageItemTaiR";
-
-      let messageItem = `
-        <li class=${liClass}>
-          <div  class="messageR">
-            <p class="message">${$messageField.val()}</p>
-            <p class="chatUsername">${nameValName}</p>
-          </div>
-        <li>
-        `
-      $messageList.append(messageItem);
-      $messageField.val('')
-      //訊息會自動滑到底
-      $(document).scrollTop($(document).height());
+    //FIELD VALUES
+    let senderName = $('#nameField option:selected').text();
+    let message = $messageField.val();
+    if (message != "") {
+      //SAVE DATA TO FIREBASE
+      messagesCollectionRef.add({
+        senderName: senderName,
+        message: message,
+        timeStamp: Date.now(),
+      });
+      $messageField.val('');
     }
-
 
   })
 
+  // A RENDER SCREEN CALLBACK THAT IS TRIGGERED FOR EACH CHAT MESSAGE
+  queryMessagesCollectionRef.onSnapshot(function (querySnapshot) {
+    $messageList.html("");
+    //MONITOR CHAT MESSAGE AND RENDER SCREEN
+    querySnapshot.forEach(function (doc) {
+      let senderName = doc.data().senderName;
+      let message = doc.data().message;
+
+      nameVal = $('#nameField option:selected').val();
+
+      if (nameVal == "Annie") {
+        if (senderName == "易安") {
+          textClass = "messageItemAnnieR";
+          messageRL = "messageR";
+        }
+        else {
+          textClass = "messageItemTaiL";
+          messageRL = "messageL";
+        }
+      }
+      else {
+        if (senderName == "易安") {
+          textClass = "messageItemAnnieL";
+          messageRL = "messageL";
+        }
+        else {
+          textClass = "messageItemTaiR";
+          messageRL = "messageR";
+        }
+      }
+
+      let messageItem = `
+      <li class=${textClass}>
+        <div class=${messageRL}>
+          <p class="message">${message}</p>
+          <p class="chatUsername">${senderName}</p>
+        </div>
+      <li>
+      `;
+      $messageList.append(messageItem);
+    });
+    //SCROLL TO BOTTOM OF MESSAGE LIST
+    $messageList[0].scrollTop = $messageList[0].scrollHeight;
+  });
 });
